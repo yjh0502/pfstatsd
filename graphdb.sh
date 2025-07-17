490b006058ac555b646596bb42e1c9580ac95f06
Jihyun Yu <yjh0502@gmail.com>
2025-07-17 17:52:02 +0900

    update graphdb.sh

--- graphdb.sh
+++ graphdb.sh
@@ -1,37 +1,46 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 OUTDIR="/var/www/html/"
 
-graph() {
+graph0() {
 	NAME="$1"
 	START="$2"
+	PREFIX="$3"
+	FILE="$4"
 	/usr/local/bin/rrdtool graph \
 		-d /var/run/rrd/rrdcached.sock \
-		"${OUTDIR}/graph-${NAME}.png" \
-		-w 600 -h 200 -a PNG \
+		"${OUTDIR}/${PREFIX}-${NAME}.png" \
+		-w 400 -h 200 -a PNG \
 		--slope-mode \
 		--start "-${START}" --end now \
 		--font DEFAULT:8: \
-		--title "bandwidth - ${NAME}" \
+		--title "bandwidth - ${PREFIX} - ${NAME}" \
 		--watermark "`date`" \
-		--vertical-label "bandwidth" \
-		--right-axis-label "packet count" \
+		--vertical-label "bytes (bytes)" \
+		--right-axis-label "packets (count)" \
 		--right-axis 0.001:0 \
 		--rigid \
-		DEF:bytes_out=/var/db/rrd/pf.rrd:bytes_out:AVERAGE \
-		DEF:bytes_in=/var/db/rrd/pf.rrd:bytes_in:AVERAGE \
-		DEF:packets_out=/var/db/rrd/pf.rrd:packets_out:AVERAGE \
-		DEF:packets_in=/var/db/rrd/pf.rrd:packets_in:AVERAGE \
+		DEF:bytes_out="${FILE}":bytes_out:AVERAGE \
+		DEF:bytes_in="${FILE}":bytes_in:AVERAGE \
+		DEF:packets_out="${FILE}":packets_out:AVERAGE \
+		DEF:packets_in="${FILE}":packets_in:AVERAGE \
 		CDEF:neg_bytes_in=packets_out,-1000,* \
 		CDEF:scale_packets_out=packets_out,1000,* \
 		CDEF:scale_packets_in=packets_in,-1000,* \
-		AREA:scale_packets_out#88ff88:"packets out" \
-		AREA:scale_packets_in#8888ff:"packets in" \
-		LINE1:bytes_out#00FF00:"bytes out" \
-		LINE1:neg_bytes_in#0000FF:"bytes in"
+		AREA:scale_packets_out#88ff88:"packets in" \
+		AREA:scale_packets_in#8888ff:"packets out" \
+		LINE1:bytes_out#00FF00:"bytes in" \
+		LINE1:neg_bytes_in#0000FF:"bytes out"
 }
 
-graph 1m 60
-graph 10m 600
-graph 1h 3600
-graph 1d 86400
+graph() {
+	PREFIX="$1"
+	FILE="$2"
+	graph0 3m 150 "$PREFIX" "$FILE"
+	graph0 15m 900 "$PREFIX" "$FILE"
+	graph0 1h 3600 "$PREFIX" "$FILE"
+	graph0 1d 86400 "$PREFIX" "$FILE"
+	graph0 28d 2422000 "$PREFIX" "$FILE"
+}
+
+graph graph /var/db/rrd/pf.rrd
